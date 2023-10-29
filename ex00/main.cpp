fbf00acf50be3a6a683f8de4f26331ac85c5ab5b
Youness Amzil <amzilyouness@gmail.com>
2023-10-29 15:13:13 +0100

    Starting...

---
+++ ex00/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************************** */
+/*																						  */
+/*                                                						d8b		888       */
+/*                                                  					Y8P		888       */
+/*                                                      						888       */
+/*		888  888	  .d88b.   	8888b.  	88888b.d88b.	88888888	888 	888       */
+/*		888  888 	d88""88b     	"88b 	888 "888 "88b    	d88P 	888 	888       */
+/*		888  888 	888  888 	.d888888 	888  888  888		d88P   	888 	888       */
+/*		Y88b 888 	Y88..88P 	888  888 	888  888  888	  d88P    	888 	888       */
+/*		"Y88888  	"Y88P"  	"Y888888 	888  888  888	88888888 	888 	888       */
+/*	  		 888																          */
+/*		Y8b d88P																          */
+/*		"Y88P"									By: yoamzil <yoamzil@student.1337.ma>     */
+/*																						  */
+/* ************************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main(void)
+{
+    Fixed   a;
+    Fixed   const b(10);
+    Fixed   const c(42.42f);
+    Fixed   const d(b);
+
+    a = Fixed(1234.4321f);
+
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+    return (0);
+}
